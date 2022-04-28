e8c945ad8d7c182d26067dad76ab945ff621b873
arturhunia@gmail.com <arturhunia@gmail.com>
2022-04-28 13:22:41 +0200

    feat: add login user

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,42 @@
 <script lang="ts">
 	import '../css/style.css';
+	import {onMount} from "svelte";
+	import {getAuth, onAuthStateChanged} from "firebase/auth";
+	import {goto} from "$app/navigation";
+	import Navbar from "$lib/components/layout/navbar.svelte"
+    import app from "./firebase.js"
+    import {isLoggedIn} from "../routes/stores/authStore.js"
+    import {Routes} from "./routes.ts";
+
+
+    onMount(() => {
+		const auth = getAuth()
+        onAuthStateChanged(auth, (user) => {
+            if(user && auth.currentUser.emailVerified) {
+                console.log("Welcome in InvestmentWallet")
+                isLoggedIn.update(() => true)
+            } else {
+                isLoggedIn.update(() => false)
+                console.log("You must login")
+                goto("/login")
+            }
+        } )
+	})
+    $:console.log("isLoggedIn", isLoggedIn)
 </script>
 
-<main>
-	<slot />
-</main>
+<div class="grid grid-cols-10">
+    {#if $isLoggedIn && window.location.href.includes(Routes.VERIFY) === false && window.location.href.includes(Routes.LOGIN) === false&& window.location.href.includes(Routes.SIGNUP) === false&& window.location.href.includes(Routes.FORGOT_PASSWORD) === false}
+        <div class="col-span-1">
+            <Navbar/>
+        </div>
+        <div class="col-span-9">
+            <slot/>
+        </div>
+    {:else}
+        <div class="col-span-10">
+            <slot/>
+        </div>
+    {/if}
+</div>
 
---
+++ src/routes/login.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import LoginForm from "../lib/components/login/LoginForm.svelte";
+
+    let email = ""
+    let password = ""
+</script>
+
+<div>
+    <LoginForm title="Login"/>
+</div>
